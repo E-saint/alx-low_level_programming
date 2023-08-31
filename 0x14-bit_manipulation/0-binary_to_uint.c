f085613bf2905d2d7a9cc8bd2df3b4e557c1f837
E-saint <esaintaic@gmail.com>
2023-08-31 07:25:10 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,19 +6,19 @@
  *
  * Return: the converted number
  */
-unsigned int binary_to_uint(const char *bn)
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int dec_val = 0;
 
-	if (!bn)
+	if (!b)
 		return (0);
 
-	for (i = 0; bn[i]; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (bn[i] < '0' || bn[i] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec_val = 2 * dec_val + (bn[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
 	return (dec_val);
