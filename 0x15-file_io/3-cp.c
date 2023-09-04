6d6b13e148c7ceecfaa77d31b1f2aff1a6d9a8cc
E-saint <esaintaic@gmail.com>
2023-09-04 06:11:59 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 char *create_buffer(char *file);
-void close_file(int f);
+void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
@@ -29,17 +29,17 @@ char *create_buffer(char *file)
 
 /**
  * close_file - Closes file descriptors.
- * @f: The file descriptor to be closed.
+ * @fd: The file descriptor to be closed.
  */
-void close_file(int f)
+void close_file(int fd)
 {
 	int c;
 
-	c = close(f);
+	c = close(fd);
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close f %d\n", f);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -58,7 +58,7 @@ void close_file(int f)
  */
 int main(int argc, char *argv[])
 {
-	int from, to, r, k;
+	int from, to, r, w;
 	char *buffer;
 
 	if (argc != 3)
@@ -81,8 +81,8 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 
-		k = write(to, buffer, r);
-		if (to == -1 || k == -1)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
@@ -100,26 +100,4 @@ int main(int argc, char *argv[])
 	close_file(to);
 
 	return (0);
-   buffer = create_buffer(argv[2]);
-        from = open(argv[1], O_RDONLY);
-        r = read(from, buffer, 1024);
-        to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-        do {
-                if (from == -1 || r == -1)
-                {
-                        dprintf(STDERR_FILENO,
-                                "Error: Can't read from file %s\n", argv[1]);
-                        free(buffer);
-                        exit(98);
-                }
-
-                k = write(to, buffer, r);
-                if (to == -1 || k == -1)
-                {
-                        dprintf(STDERR_FILENO,
-                                "Error: Can't write to %s\n", argv[2]);
-                        free(buffer);
-                        exit(99);
-                }}
 }
